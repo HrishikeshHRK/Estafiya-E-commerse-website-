b6485b858e5827b2db3693a74f26872ff0038115
HrishikeshHRK <ankaran87@gmail.com>
2022-12-14 14:12:09 +0530

    checkout done

--- ecomapp/models.py
+++ ecomapp/models.py
@@ -22,9 +22,15 @@ class Product(models.Model):
         return self.name  
 
     @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
 
 class Order(models.Model):
-    custumer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
     date_ordered= models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False)
     transaction_id = models.CharField(max_length=100, null=True)
@@ -32,6 +38,18 @@ class Order(models.Model):
 
     def __str__(self):
             return str(self.id)
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
 
 class orderitem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
@@ -39,6 +57,10 @@ class orderitem(models.Model):
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added= models.DateTimeField(auto_now_add=True)
 
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
